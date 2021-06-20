6c925002000c97b1ef42d68d9944c983c3051e11
larell mcbride <mcbridelarell@gmail.com>
2021-06-19 20:10:09 -0400

    Started inital setup. issue with JFrame not showing window. will fix tomorrtow.

---
+++ NewProject/src/main/UI.java
@@ -0,0 +1,33 @@
+package main;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class UI {
+    GameManager gm;
+
+    JFrame window;
+
+    public UI(GameManager gm) {
+
+        this.gm = gm;
+
+        creatMainField();
+
+        window.setVisible(true);
+        
+    }
+
+    public void creatMainField() {
+
+        window = new JFrame();
+        window.setSize(800,600);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getConentPane().setBackground(color.black);
+        window.setLayout(null);
+    }
+
+
+    
+}
